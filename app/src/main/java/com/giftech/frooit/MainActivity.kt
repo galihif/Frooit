ab94e10673826dc761b20214ef569a436f7ad3cd
galihif <galihindra2401@mail.ugm.ac.id>
2021-10-28 21:19:38 +0700

    add menu layout

--- app/src/main/java/com/giftech/frooit/MainActivity.kt
+++ app/src/main/java/com/giftech/frooit/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.giftech.frooit
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
