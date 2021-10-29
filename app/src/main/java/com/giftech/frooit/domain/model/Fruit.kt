74b9286f893cc4d7a0c31d784650b17af8344fc3
galihif <galihindra2401@mail.ugm.ac.id>
2021-10-29 19:14:41 +0700

    make Fruit.kt parcelable

--- app/src/main/java/com/giftech/frooit/domain/model/Fruit.kt
+++ app/src/main/java/com/giftech/frooit/domain/model/Fruit.kt
@@ -1,8 +1,12 @@
 package com.giftech.frooit.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Fruit(
     var id:Int = 0,
     var name:String = "",
     var family:String = "",
     var genus:String = ""
-)
+):Parcelable
