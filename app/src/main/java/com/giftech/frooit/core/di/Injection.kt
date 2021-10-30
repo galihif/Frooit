09ec247a49bc9f284884b906d5e33bbfb5c57c59
galihif <galihindra2401@mail.ugm.ac.id>
2021-10-30 09:57:03 +0700

    inject localdatasource and appexec

--- app/src/main/java/com/giftech/frooit/core/di/Injection.kt
+++ app/src/main/java/com/giftech/frooit/core/di/Injection.kt
@@ -2,16 +2,22 @@ package com.giftech.frooit.core.di
 
 import android.content.Context
 import com.giftech.frooit.core.data.FruitRepository
+import com.giftech.frooit.core.data.source.local.LocalDataSource
+import com.giftech.frooit.core.data.source.local.room.FruitDatabase
 import com.giftech.frooit.core.data.source.remote.RemoteDataSource
 import com.giftech.frooit.core.data.source.remote.network.ApiConfig
+import com.giftech.frooit.core.utils.AppExecutors
 
 object Injection {
 
     fun provideRepository(context: Context):FruitRepository{
-        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
-        val fruitRepository = FruitRepository.getInstance(remoteDataSource)
+        val database = FruitDatabase.getInstance(context)
 
-        return fruitRepository
+        val localDataSource = LocalDataSource.getInstance(database.fruitDao())
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
+        val appExecutors = AppExecutors()
+
+        return FruitRepository.getInstance(localDataSource,remoteDataSource, appExecutors)
     }
 
 }
