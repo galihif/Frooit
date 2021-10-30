1dc8f106e5aa30362c3d9151fec9a1c27348bf3c
galihif <galihindra2401@mail.ugm.ac.id>
2021-10-30 09:54:29 +0700

    change output type

--- app/src/main/java/com/giftech/frooit/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/giftech/frooit/core/data/source/remote/RemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.giftech.frooit.core.data.source.remote
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.giftech.frooit.core.data.source.remote.network.ApiResponse
 import com.giftech.frooit.core.data.source.remote.network.ApiService
 import com.giftech.frooit.core.data.source.remote.response.FruitResponse
 import retrofit2.Call
@@ -20,8 +21,8 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
             }
     }
 
-    fun getListFruit():LiveData<List<FruitResponse>> {
-        val resultData = MutableLiveData<List<FruitResponse>>()
+    fun getListFruit():LiveData<ApiResponse<List<FruitResponse>>> {
+        val resultData = MutableLiveData<ApiResponse<List<FruitResponse>>>()
 
         apiService.getListFruit().enqueue(object : retrofit2.Callback<List<FruitResponse>>{
             override fun onResponse(
@@ -30,11 +31,12 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
             ) {
                 if(response.isSuccessful){
                     val res = response.body()
-                    resultData.value = res
+                    resultData.value = if (res != null) ApiResponse.Success(res) else ApiResponse.Empty
                 }
             }
 
             override fun onFailure(call: Call<List<FruitResponse>>, t: Throwable) {
+                resultData.value = ApiResponse.Error(t.message.toString())
                 Log.e("RemoteDataSource", t.message.toString())
             }
 
