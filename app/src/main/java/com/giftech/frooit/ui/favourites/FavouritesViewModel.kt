edb0a62280aaff5d491b24e394ce348fbb88d8bd
galihif <galihindra2401@mail.ugm.ac.id>
2021-10-31 11:16:51 +0700

    convert flow to livedata

--- app/src/main/java/com/giftech/frooit/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/giftech/frooit/ui/favourites/FavouritesViewModel.kt
@@ -2,11 +2,12 @@ package com.giftech.frooit.ui.favourites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.giftech.frooit.core.domain.model.Fruit
 import com.giftech.frooit.core.domain.usecase.FruitUseCase
 
 class FavouritesViewModel(private val fruitUseCase: FruitUseCase):ViewModel() {
 
-    fun getFavoriteFruit():LiveData<List<Fruit>> = fruitUseCase.getFavoriteFruit()
+    fun getFavoriteFruit():LiveData<List<Fruit>> = fruitUseCase.getFavoriteFruit().asLiveData()
 
 }
--- app/src/main/java/com/giftech/frooit/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/giftech/frooit/ui/home/HomeViewModel.kt
@@ -2,12 +2,13 @@ package com.giftech.frooit.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.giftech.frooit.core.data.Resource
 import com.giftech.frooit.core.domain.model.Fruit
 import com.giftech.frooit.core.domain.usecase.FruitUseCase
 
 class HomeViewModel(private val fruitUseCase: FruitUseCase):ViewModel() {
 
-    fun getListFruit():LiveData<Resource<List<Fruit>>> = fruitUseCase.getListFruit()
+    fun getListFruit():LiveData<Resource<List<Fruit>>> = fruitUseCase.getListFruit().asLiveData()
 
 }
